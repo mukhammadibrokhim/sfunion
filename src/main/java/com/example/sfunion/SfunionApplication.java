f1aee43422a15cb9de2d508696a54e0d3b4b14b6
mukhammadibrokhim <tursunboyev06061999@gmail.com>
2021-10-27 13:46:52 +0500

    initial commit

---
+++ src/main/java/com/example/sfunion/SfunionApplication.java
@@ -0,0 +1,13 @@
+package com.example.sfunion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfunionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfunionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sfunion/SfunionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sfunion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfunionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
